9a507b2f9ea70f911fb080602c965c4fdc4c07c3
rick <technoweenie@gmail.com>
2011-02-28 03:30:00 +0800

    update response adapters for experimental middleware api

--- lib/faraday/mashify.rb
+++ lib/faraday/mashify.rb
@@ -8,20 +8,13 @@ module Faraday
       self.load_error = error
     end
 
-    def self.register_on_complete(env)
-      env[:response].on_complete do |response|
-        response_body = response[:body]
-        if response_body.is_a?(Hash)
-          response[:body] = ::Hashie::Mash.new(response_body)
-        elsif response_body.is_a?(Array)
-          response[:body] = response_body.map{|item| item.is_a?(Hash) ? ::Hashie::Mash.new(item) : item}
-        end
+    def on_complete(env)
+      response_body = env[:body]
+      if response_body.is_a?(Hash)
+        env[:body] = ::Hashie::Mash.new(response_body)
+      elsif response_body.is_a?(Array)
+        env[:body] = response_body.map{|item| item.is_a?(Hash) ? ::Hashie::Mash.new(item) : item}
       end
-    end
-
-    def initialize(app)
-      super
-      @parser = nil
     end
   end
 end
