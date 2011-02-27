7b8b9661999be3a2a76f8c1d7c8891cf5e0e88e5
Erik Michaels-Ober <sferik@gmail.com>
2011-02-27 11:15:51 -0800

    Remove old tests

--- test/helper.rb
+++
@@ -1,9 +0,0 @@
-require 'simplecov'
-SimpleCov.start do
-  add_group 'Libraries', 'lib'
-end
-
-require 'test/unit'
-require 'shoulda'
-
-require File.expand_path('../../lib/faraday_middleware', __FILE__)
