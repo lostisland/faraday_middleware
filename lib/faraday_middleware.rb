ee237b631d94fb6c5a86ab247940a8f9883bee4d
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-03-03 12:33:01 +0100

    lets autoload, this reduces what is loaded in when faraday_middleware is required

--- lib/faraday_middleware.rb
+++ lib/faraday_middleware.rb
@@ -1,4 +1,11 @@
-require 'faraday/mashify'
-require 'faraday/oauth2'
-require 'faraday/parse_json'
-require 'faraday/parse_xml'
+require 'faraday'
+
+class Faraday::Request
+  autoload :OAuth2, 'faraday/oauth2'
+end
+
+class Faraday::Response
+  autoload :Mashify,   'faraday/mashify'
+  autoload :ParseJson, 'faraday/parse_json'
+  autoload :ParseXml,  'faraday/parse_xml'
+end
