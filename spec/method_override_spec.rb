ce247d38ed4d3ab6e2247500aa7b7ea8242457b4
Paul Annesley <paul@annesley.cc>
2012-08-30 11:03:11 +1000

    method_override_spec spec simplified; assertions more direct.

--- spec/method_override_spec.rb
+++ spec/method_override_spec.rb
@@ -4,32 +4,31 @@ require 'faraday_middleware/request/method_override'
 describe FaradayMiddleware::MethodOverride do
 
   subject do
-    FaradayMiddleware::MethodOverride.new(app, *options)
+    FaradayMiddleware::MethodOverride.new(
+      lambda { |env| env },
+      *options
+    )
   end
-
-  # A null Faraday app which returns the unmodified env.
-  let(:app) { lambda { |env| env } }
 
   # A minimal Faraday request env for the given HTTP method.
   def env_for_method(method)
     { :method => method, :request_headers => Faraday::Utils::Headers.new }
   end
 
-  # The after-middleware HTTP method and override header value
-  # for the given request method.
-  def method_and_header_for(method)
-    request = subject.call(env_for_method(method))
-    [request[:method], request[:request_headers]["X-Http-Method-Override"]]
-  end
-
   # A simple test, before getting into more complicated declarative tests.
   describe "for a simple use case" do
     let(:options) { [:patch] }
+
     it "rewrites a PATCH request" do
-      method_and_header_for(:patch).should == [:post, "PATCH"]
+      env = subject.call(env_for_method(:patch))
+      env[:method].should == :post
+      env[:request_headers]["X-Http-Method-Override"].should == "PATCH"
     end
+
     it "does not rewrite a GET request" do
-      method_and_header_for(:get).should == [:get, nil]
+      env = subject.call(env_for_method(:get))
+      env[:method].should == :get
+      env[:request_headers].should_not have_key("X-Http-Method-Override")
     end
   end
 
@@ -76,13 +75,21 @@ describe FaradayMiddleware::MethodOverride do
       expectations.each do |method, should_override|
         method_up = method.to_s.upcase
         if should_override
+
           it "sends #{method_up} as POST with #{method_up} in header" do
-            method_and_header_for(method).should == [:post, method_up]
+            env = subject.call(env_for_method(method))
+            env[:method].should == :post
+            env[:request_headers]["X-Http-Method-Override"].should == method_up
           end
+
         else
+
           it "sends #{method_up} unmodified with no header" do
-            method_and_header_for(method).should == [method, nil]
+            env = subject.call(env_for_method(method))
+            env[:method].should == method
+            env[:request_headers].should_not have_key("X-Http-Method-Override")
           end
+
         end
       end
 
