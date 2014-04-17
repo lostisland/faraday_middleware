ea48a833ee9447055f2e8cf3f6b42cd9d0904312
Mislav Marohnić <mislav.marohnic@gmail.com>
2014-04-17 03:06:01 +0200

    Improve mashify/rashify specs
    The specs used to be isolated from the `call(env)` cycle by means of
    using the `on_complete` method directly. Since that method is not
    considered part of the public API of Faraday::Response::Middleware, this
    switches away from using that method and makes the middleware process
    responses the standard way, via `call()`.
    
    This gets rid of the need for "integration tests" because the middleware
    is exercised exactly as it would during a real request/response cycle.

--- spec/mashify_spec.rb
+++ spec/mashify_spec.rb
@@ -2,98 +2,69 @@ require 'helper'
 require 'faraday_middleware/response/mashify'
 
 describe FaradayMiddleware::Mashify do
-  context "during configuration" do
-    it "allows for a custom Mash class to be set" do
-      expect(described_class).to respond_to(:mash_class)
-      expect(described_class).to respond_to(:mash_class=)
-    end
-  end
+  MyMash = Struct.new(:body)
 
-  context "when used" do
-    before(:each) { described_class.mash_class = ::Hashie::Mash }
-    let(:mashify) { described_class.new }
-
+  context "when used", :type => :response do
     it "creates a Hashie::Mash from the body" do
-      env = { :body => { "name" => "Erik Michaels-Ober", "username" => "sferik" } }
-      me  = mashify.on_complete(env)
-      expect(me.class).to eq(Hashie::Mash)
+      body = { "name" => "Erik Michaels-Ober", "username" => "sferik" }
+      me = process(body).body
+      expect(me.name).to eq("Erik Michaels-Ober")
+      expect(me.username).to eq("sferik")
     end
 
     it "handles strings" do
-      env = { :body => "Most amazing string EVER" }
-      me  = mashify.on_complete(env)
+      body = "Most amazing string EVER"
+      me = process(body).body
       expect(me).to eq("Most amazing string EVER")
     end
 
     it "handles arrays" do
-      env = { :body => [123, 456] }
-      values = mashify.on_complete(env)
-      expect(values.first).to eq(123)
-      expect(values.last).to eq(456)
+      body = [123, 456]
+      values = process(body).body
+      expect(values).to eq([123, 456])
     end
 
     it "handles arrays of hashes" do
-      env = { :body => [{ "username" => "sferik" }, { "username" => "pengwynn" }] }
-      us  = mashify.on_complete(env)
+      body = [{ "username" => "sferik" }, { "username" => "pengwynn" }]
+      us = process(body).body
       expect(us.first.username).to eq('sferik')
       expect(us.last.username).to eq('pengwynn')
     end
 
     it "handles nested arrays of hashes" do
-      env = { :body => [[{ "username" => "sferik" }, { "username" => "pengwynn" }]] }
-      us  = mashify.on_complete(env)
-      expect(us.first.first.username).to eq('sferik')
-      expect(us.first.last.username).to eq('pengwynn')
+      body = [[{ "username" => "sferik" }, { "username" => "pengwynn" }]]
+      us = process(body).body.first
+      expect(us.first.username).to eq('sferik')
+      expect(us.last.username).to eq('pengwynn')
     end
 
     it "handles mixed arrays" do
-      env = { :body => [123, { "username" => "sferik" }, 456] }
-      values = mashify.on_complete(env)
+      body = [123, { "username" => "sferik" }, 456]
+      values = process(body).body
       expect(values.first).to eq(123)
       expect(values.last).to eq(456)
       expect(values[1].username).to eq('sferik')
     end
 
     it "allows for use of custom Mash subclasses at the class level" do
-      class MyMash < ::Hashie::Mash; end
+      original_class = described_class.mash_class
       described_class.mash_class = MyMash
 
-      env = { :body => { "name" => "Erik Michaels-Ober", "username" => "sferik" } }
-      me  = mashify.on_complete(env)
-
-      expect(me.class).to eq(MyMash)
-    end
-
-    it "allows for use of custom Mash subclasses at the instance level" do
-      class MyMash < ::Hashie::Mash; end
-      mashify = described_class.new(nil, :mash_class => MyMash)
-
-      env = { :body => { "name" => "Erik Michaels-Ober", "username" => "sferik" } }
-      me  = mashify.on_complete(env)
-
-      expect(me.class).to eq(MyMash)
+      begin
+        me = process({}).body
+        expect(me).to be_instance_of(MyMash)
+      ensure
+        described_class.mash_class = original_class
+      end
     end
   end
 
-  context "integration test" do
-    let(:stubs) { Faraday::Adapter::Test::Stubs.new }
-    let(:connection) do
-      Faraday::Connection.new do |builder|
-        builder.adapter :test, stubs
-        builder.use described_class
-      end
-    end
+  context "custom mash subclass", :type => :response do
+    let(:options) { {:mash_class => MyMash} }
 
-    # although it is not good practice to pass a hash as the body, if we add ParseJson
-    # to the middleware stack we end up testing two middlewares instead of one
-    it "creates a Hash from the body" do
-      stubs.get('/hash') {
-        data = { 'name' => 'Erik Michaels-Ober', 'username' => 'sferik' }
-        [200, {'content-type' => 'application/json; charset=utf-8'}, data]
-      }
-      me = connection.get('/hash').body
-      expect(me.name).to eq('Erik Michaels-Ober')
-      expect(me.username).to eq('sferik')
+    it "instance level" do
+      me = process({}).body
+        expect(me).to be_instance_of(MyMash)
     end
   end
 end
--- spec/rashify_spec.rb
+++ spec/rashify_spec.rb
@@ -2,71 +2,46 @@ require 'helper'
 require 'faraday_middleware/response/rashify'
 
 describe FaradayMiddleware::Rashify do
-
-  context "when used" do
-    let(:rashify) { described_class.new }
-
+  context "when used", :type => :response do
     it "creates a Hashie::Rash from the body" do
-      env = { :body => { "name" => "Erik Michaels-Ober", "username" => "sferik" } }
-      me  = rashify.on_complete(env)
-      expect(me.class).to eq(Hashie::Rash)
+      body = { "name" => "Erik Michaels-Ober", "username" => "sferik" }
+      me = process(body).body
+      expect(me.name).to eq("Erik Michaels-Ober")
+      expect(me.username).to eq("sferik")
     end
 
     it "handles strings" do
-      env = { :body => "Most amazing string EVER" }
-      me  = rashify.on_complete(env)
+      body = "Most amazing string EVER"
+      me = process(body).body
       expect(me).to eq("Most amazing string EVER")
     end
 
     it "handles hashes and decamelcase the keys" do
-      env = { :body => { "name" => "Erik Michaels-Ober", "userName" => "sferik" } }
-      me  = rashify.on_complete(env)
+      body = { "name" => "Erik Michaels-Ober", "userName" => "sferik" }
+      me = process(body).body
       expect(me.name).to eq('Erik Michaels-Ober')
       expect(me.user_name).to eq('sferik')
     end
 
     it "handles arrays" do
-      env = { :body => [123, 456] }
-      values = rashify.on_complete(env)
-      expect(values.first).to eq(123)
-      expect(values.last).to eq(456)
+      body = [123, 456]
+      values = process(body).body
+      expect(values).to eq([123, 456])
     end
 
     it "handles arrays of hashes" do
-      env = { :body => [{ "username" => "sferik" }, { "username" => "pengwynn" }] }
-      us  = rashify.on_complete(env)
+      body = [{ "username" => "sferik" }, { "username" => "pengwynn" }]
+      us = process(body).body
       expect(us.first.username).to eq('sferik')
       expect(us.last.username).to eq('pengwynn')
     end
 
     it "handles mixed arrays" do
-      env = { :body => [123, { "username" => "sferik" }, 456] }
-      values = rashify.on_complete(env)
+      body = [123, { "username" => "sferik" }, 456]
+      values = process(body).body
       expect(values.first).to eq(123)
       expect(values.last).to eq(456)
       expect(values[1].username).to eq('sferik')
-    end
-  end
-
-  context "integration test" do
-    let(:stubs) { Faraday::Adapter::Test::Stubs.new }
-    let(:connection) do
-      Faraday::Connection.new do |builder|
-        builder.adapter :test, stubs
-        builder.use described_class
-      end
-    end
-
-    # although it is not good practice to pass a hash as the body, if we add ParseJson
-    # to the middleware stack we end up testing two middlewares instead of one
-    it "creates a Hash from the body" do
-      stubs.get('/hash') {
-        data = { 'name' => 'Erik Michaels-Ober', 'username' => 'sferik' }
-        [200, {'content-type' => 'application/json; charset=utf-8'}, data]
-      }
-      me = connection.get('/hash').body
-      expect(me.name).to eq('Erik Michaels-Ober')
-      expect(me.username).to eq('sferik')
     end
   end
 end
