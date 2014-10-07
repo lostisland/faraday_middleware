4759f1e144ded2e74694cdfbe0a202342d81ea45
Eric Krause <eric.krause@gmail.com>
2014-10-07 14:15:05 -0600

    support the .NET DateTime Wire Format

--- lib/faraday_middleware/response/parse_dates.rb
+++ lib/faraday_middleware/response/parse_dates.rb
@@ -5,6 +5,7 @@ module FaradayMiddleware
   # Parse dates from response body
   class ParseDates < ::Faraday::Response::Middleware
     ISO_DATE_FORMAT = /\A\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z\Z/m
+    MS_DATE_FORMAT = /\/Date\((\d+)\)\//
 
     def initialize(app, options = {})
       @regexp = options[:match] || ISO_DATE_FORMAT
@@ -31,6 +32,8 @@ module FaradayMiddleware
         end
       when @regexp
         Time.parse(value)
+      when MS_DATE_FORMAT
+        Time.at(value.match(MS_DATE_FORMAT)[1].to_i/1000).utc
       else
         value
       end
