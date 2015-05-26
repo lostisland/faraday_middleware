bd6dc4062150f09ae168f0776ff28f20ae84fe97
Jens Finkhaeuser <jens@finkhaeuser.de>
2015-05-26 16:03:09 +0200

    Test cases covering specifically utf-8 and non-utf-8+non-ASCII strings.

--- spec/unit/encode_json_spec.rb
+++ spec/unit/encode_json_spec.rb
@@ -12,6 +12,7 @@ describe FaradayMiddleware::EncodeJson do
 
   def result_body() result[:body] end
   def result_type() result[:request_headers]['content-type'] end
+  def result_length() result[:request_headers]['content-length'].to_i end
 
   context "no body" do
     let(:result) { process(nil) }
@@ -90,6 +91,70 @@ describe FaradayMiddleware::EncodeJson do
 
     it "doesn't change content type" do
       expect(result_type).to eq('application/xml; charset=utf-8')
+    end
+  end
+
+  context "utf-8 in string body" do
+    let(:result) { process('{"a":"ä"}') }
+
+    it "doesn't change body" do
+      expect(result_body).to eq('{"a":"ä"}')
+    end
+
+    it "adds content type" do
+      expect(result_type).to eq('application/json; charset=utf-8')
+    end
+
+    it "adds content length" do
+      expect(result_length).to eq(10)
+    end
+  end
+
+  context "utf-8 in object body" do
+    let(:result) { process({:a => "ä"}) }
+
+    it "encodes body" do
+      expect(result_body).to eq('{"a":"ä"}')
+    end
+
+    it "adds content type" do
+      expect(result_type).to eq('application/json; charset=utf-8')
+    end
+
+    it "adds content length" do
+      expect(result_length).to eq(10)
+    end
+  end
+
+  context "non-utf-8 in string body" do
+    let(:result) { process('{"a":"ä"}'.encode('iso-8859-15')) }
+
+    it "doesn't change body" do
+      expect(result_body).to eq('{"a":"ä"}')
+    end
+
+    it "adds content type" do
+      expect(result_type).to eq('application/json; charset=utf-8')
+    end
+
+    it "adds content length" do
+      expect(result_length).to eq(10)
+    end
+  end
+
+  context "non-utf-8 in object body" do
+    let(:result) { process({:a => "ä".encode('iso-8859-15')}) }
+
+    it "encodes body" do
+      expect(result_body).to eq('{"a":"ä"}')
+    end
+
+    it "adds content type" do
+      expect(result_type).to eq('application/json; charset=utf-8')
+    end
+
+    it "adds content length" do
+      expect(result_length).to eq(10)
     end
   end
 end
