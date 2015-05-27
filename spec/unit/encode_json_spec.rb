d82f549cdd01fdacd1e41e05deb490612447ee4c
Jens Finkhaeuser <jens@finkhaeuser.de>
2015-05-27 12:07:18 +0200

    Thanks to Ruby 1.8 support, there was a need for a character encoding method in the specs to generate test data.
    Also, test cases are changed in that
    - they must succeed for all Ruby versions with a given charset in the
    content type header
    - they may succeed for Ruby > 1.8 without a charset in the content type
    header

--- spec/unit/encode_json_spec.rb
+++ spec/unit/encode_json_spec.rb
@@ -3,6 +3,18 @@
 require 'helper'
 require 'faraday_middleware/request/encode_json'
 
+def encode(str, encoding)
+  if RUBY_VERSION.start_with?("1.8")
+    # Ruby 1.8 stores strings as bytes. Since this file is in UTF-8, we'll
+    # fix the FROM encoding to UTF-8.
+    require 'iconv'
+    return ::Iconv.conv(encoding, 'UTF-8', str)
+  else
+    # Yay, sanity!
+    return str.encode(encoding)
+  end
+end
+
 describe FaradayMiddleware::EncodeJson do
   let(:middleware) { described_class.new(lambda{|env| env}) }
 
@@ -96,14 +108,19 @@ describe FaradayMiddleware::EncodeJson do
     end
   end
 
+  ### Unicode test cases
+  # Ruby 1.8 will almost certainly fail if there is no charset given in a header.
+  # In Ruby >1.8, we have some more methods for guessing well.
+
+  ### All Ruby versions should work with a charset given.
   context "utf-8 in string body" do
-    let(:result) { process('{"a":"ä"}') }
+    let(:result) { process('{"a":"ä"}', 'application/json; charset=utf-8') }
 
     it "doesn't change body" do
       expect(result_body).to eq('{"a":"ä"}')
     end
 
-    it "adds content type" do
+    it "doesn't change content type" do
       expect(result_type).to eq('application/json; charset=utf-8')
     end
 
@@ -113,13 +130,13 @@ describe FaradayMiddleware::EncodeJson do
   end
 
   context "utf-8 in object body" do
-    let(:result) { process({:a => "ä"}) }
+    let(:result) { process({:a => "ä"}, 'application/json; charset=utf-8') }
 
     it "encodes body" do
       expect(result_body).to eq('{"a":"ä"}')
     end
 
-    it "adds content type" do
+    it "doesn't change content type" do
       expect(result_type).to eq('application/json; charset=utf-8')
     end
 
@@ -129,13 +146,15 @@ describe FaradayMiddleware::EncodeJson do
   end
 
   context "non-unicode in string body" do
-    let(:result) { process('{"a":"ä"}'.encode('iso-8859-15')) }
+    let(:result) {
+      process(encode('{"a":"ä"}', 'iso-8859-15'), 'application/json; charset=iso-8859-15')
+    }
 
-    it "doesn't change body" do
+    it "changes body" do
       expect(result_body).to eq('{"a":"ä"}')
     end
 
-    it "adds content type" do
+    it "changes content type" do
       expect(result_type).to eq('application/json; charset=utf-8')
     end
 
@@ -145,13 +164,15 @@ describe FaradayMiddleware::EncodeJson do
   end
 
   context "non-unicode in object body" do
-    let(:result) { process({:a => "ä".encode('iso-8859-15')}) }
+    let(:result) {
+      process({:a => encode('ä', 'iso-8859-15')}, 'application/json; charset=iso-8859-15')
+    }
 
     it "encodes body" do
       expect(result_body).to eq('{"a":"ä"}')
     end
 
-    it "adds content type" do
+    it "changes content type" do
       expect(result_type).to eq('application/json; charset=utf-8')
     end
 
@@ -161,13 +182,15 @@ describe FaradayMiddleware::EncodeJson do
   end
 
   context "non-utf-8 in string body" do
-    let(:result) { process('{"a":"ä"}'.encode('utf-16BE')) }
+    let(:result) {
+      process(encode('{"a":"ä"}', 'utf-16be'), 'application/json; charset=utf-16be')
+    }
 
-    it "doesn't change body" do
+    it "changes body" do
       expect(result_body).to eq('{"a":"ä"}')
     end
 
-    it "adds content type" do
+    it "changes content type" do
       expect(result_type).to eq('application/json; charset=utf-8')
     end
 
@@ -177,13 +200,15 @@ describe FaradayMiddleware::EncodeJson do
   end
 
   context "non-utf-8 in object body" do
-    let(:result) { process({:a => "ä".encode('utf-16le')}) }
+    let(:result) {
+      process({:a => encode('ä', 'utf-16le')}, 'application/json; charset=utf-16le')
+    }
 
     it "encodes body" do
       expect(result_body).to eq('{"a":"ä"}')
     end
 
-    it "adds content type" do
+    it "changes content type" do
       expect(result_type).to eq('application/json; charset=utf-8')
     end
 
@@ -192,4 +217,120 @@ describe FaradayMiddleware::EncodeJson do
     end
   end
 
+
+  ### Ruby versions > 1.8 should be able to guess missing charsets at times.
+  if not RUBY_VERSION.start_with?("1.8")
+    context "utf-8 in string body without content type" do
+      let(:result) { process('{"a":"ä"}') }
+
+      it "doesn't change body" do
+        expect(result_body).to eq('{"a":"ä"}')
+      end
+
+      it "adds content type" do
+        expect(result_type).to eq('application/json; charset=utf-8')
+      end
+
+      it "adds content length" do
+        expect(result_length).to eq(10)
+      end
+    end
+
+    context "utf-8 in object body without content type" do
+      let(:result) { process({:a => "ä"}) }
+
+      it "encodes body" do
+        expect(result_body).to eq('{"a":"ä"}')
+      end
+
+      it "adds content type" do
+        expect(result_type).to eq('application/json; charset=utf-8')
+      end
+
+      it "adds content length" do
+        expect(result_length).to eq(10)
+      end
+    end
+
+    context "non-unicode in string body without content type" do
+      let(:result) {
+        process(encode('{"a":"ä"}', 'iso-8859-15'))
+      }
+
+      it "doesn't change body" do
+        expect(result_body).to eq('{"a":"ä"}')
+      end
+
+      it "adds content type" do
+        expect(result_type).to eq('application/json; charset=utf-8')
+      end
+
+      it "adds content length" do
+        expect(result_length).to eq(10)
+      end
+    end
+
+    context "non-unicode in object body without content type" do
+      let(:result) {
+        process({:a => encode('ä', 'iso-8859-15')})
+      }
+
+      it "encodes body" do
+        expect(result_body).to eq('{"a":"ä"}')
+      end
+
+      it "adds content type" do
+        expect(result_type).to eq('application/json; charset=utf-8')
+      end
+
+      it "adds content length" do
+        expect(result_length).to eq(10)
+      end
+    end
+
+    context "non-utf-8 in string body without content type" do
+      let(:result) {
+        process(encode('{"a":"ä"}', 'utf-16be'))
+      }
+
+
+      it "doesn't change body" do
+        expect(result_body).to eq('{"a":"ä"}')
+      end
+
+      it "adds content type" do
+        expect(result_type).to eq('application/json; charset=utf-8')
+      end
+
+      it "adds content length" do
+        expect(result_length).to eq(10)
+      end
+    end
+
+    context "non-utf-8 in object body without content type" do
+      let(:result) {
+        process({:a => encode('ä', 'utf-16le')})
+      }
+
+      it "encodes body" do
+        expect(result_body).to eq('{"a":"ä"}')
+      end
+
+      it "adds content type" do
+        expect(result_type).to eq('application/json; charset=utf-8')
+      end
+
+      it "adds content length" do
+        expect(result_length).to eq(10)
+      end
+    end
+
+    context "mismatching charset and encoding" do
+      it "should fail with an exception" do
+        expect {
+          process(encode('{"a":"ä"}', 'iso-8859-15'), 'application/json; charset=utf-8')
+        }.to raise_exception
+      end
+    end
+  end
 end
