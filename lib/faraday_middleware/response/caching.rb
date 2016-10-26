9389fba5f5a15282e50f522271c7372f578b7ba1
Kliment Mamykin <kmamyk@gmail.com>
2016-10-26 12:08:33 -0400

    Using block form of .on_complete which yields response

--- lib/faraday_middleware/response/caching.rb
+++ lib/faraday_middleware/response/caching.rb
@@ -78,14 +78,13 @@ module FaradayMiddleware
       if cached_response = cache.read(key)
         finalize_response(cached_response, env)
       else
-        response = @app.call(env)
         # response.status is nil at this point, any checks need to be done inside on_complete block
-        response.on_complete do
+        @app.call(env).on_complete do |response|
           if CACHEABLE_STATUS_CODES.include?(response.status)
             cache.write(key, response)
           end
+          response
         end
-        response
       end
     end
 
