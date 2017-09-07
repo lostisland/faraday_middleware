afa7724c696ac44e4d516f536497922c550d0393
Olle Jonsson <olle.jonsson@gmail.com>
2017-09-07 18:25:21 +0200

    [docs] Avoid YARD warnings: define empty Faraday module, classes (#170)

--- lib/faraday_middleware/backwards_compatibility.rb
+++ lib/faraday_middleware/backwards_compatibility.rb
@@ -1,15 +1,18 @@
 # deprecated constants
 
-Faraday::Request.class_eval do
-  autoload :OAuth,        'faraday_middleware/request/oauth'
-  autoload :OAuth2,       'faraday_middleware/request/oauth2'
+module Faraday
+  class Request
+    autoload :OAuth,        'faraday_middleware/request/oauth'
+    autoload :OAuth2,       'faraday_middleware/request/oauth2'
+  end
+
+  class Response
+    autoload :Mashify,      'faraday_middleware/response/mashify'
+    autoload :Rashify,      'faraday_middleware/response/rashify'
+    autoload :ParseJson,    'faraday_middleware/response/parse_json'
+    autoload :ParseXml,     'faraday_middleware/response/parse_xml'
+    autoload :ParseMarshal, 'faraday_middleware/response/parse_marshal'
+    autoload :ParseYaml,    'faraday_middleware/response/parse_yaml'
+  end
 end
 
-Faraday::Response.class_eval do
-  autoload :Mashify,      'faraday_middleware/response/mashify'
-  autoload :Rashify,      'faraday_middleware/response/rashify'
-  autoload :ParseJson,    'faraday_middleware/response/parse_json'
-  autoload :ParseXml,     'faraday_middleware/response/parse_xml'
-  autoload :ParseMarshal, 'faraday_middleware/response/parse_marshal'
-  autoload :ParseYaml,    'faraday_middleware/response/parse_yaml'
-end
