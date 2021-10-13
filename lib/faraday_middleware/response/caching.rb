784b4f83e39d1559cf6dc6fbbc7afb1fa1797cd7
Wei Jun <wjloh91@gmail.com>
2021-10-13 23:08:33 +0800

    Allow users to define custom sub-set of Cacheable Status Code when using Faraday Middleware Cache (#275)

--- lib/faraday_middleware/response/caching.rb
+++ lib/faraday_middleware/response/caching.rb
@@ -27,14 +27,16 @@ module FaradayMiddleware
     # cache   - An object that responds to read and write (default: nil).
     # options - An options Hash (default: {}):
     #           :ignore_params - String name or Array names of query
-    #                            params that should be ignored when forming
-    #                            the cache key (default: []).
+    #                                    params that should be ignored when forming
+    #                                    the cache key (default: []).
     #           :write_options - Hash of settings that should be passed as the
-    #                            third options parameter to the cache's #write
-    #                            method. If not specified, no options parameter
-    #                            will be passed.
+    #                                    third options parameter to the cache's #write
+    #                                    method. If not specified, no options parameter
+    #                                    will be passed.
     #           :full_key      - Boolean - use full URL as cache key:
-    #                            (url.host + url.request_uri)
+    #                                    (url.host + url.request_uri)
+    #           :status_codes  - Array of http status code to be cache
+    #                                    (default: CACHEABLE_STATUS_CODE)
     #
     # Yields if no cache is given. The block should return a cache object.
     def initialize(app, cache = nil, options = {})
@@ -86,6 +88,13 @@ module FaradayMiddleware
       @full_key ||= @options[:full_key]
     end
 
+    def custom_status_codes
+      @custom_status_codes ||= begin
+        codes = CACHEABLE_STATUS_CODES & Array(@options[:status_codes]).map(&:to_i)
+        codes.any? ? codes : CACHEABLE_STATUS_CODES
+      end
+    end
+
     def cache_on_complete(env)
       key = cache_key(env)
       if (cached_response = cache.read(key))
@@ -101,7 +110,7 @@ module FaradayMiddleware
     end
 
     def store_response_in_cache(key, response)
-      return unless CACHEABLE_STATUS_CODES.include?(response.status)
+      return unless custom_status_codes.include?(response.status)
 
       if @options[:write_options]
         cache.write(key, response, @options[:write_options])
